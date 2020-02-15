13c634bcb65eac2c7dc7d3e1ab0ce8f17b238102
virgile <virgile.oxaran@gmail.com>
2020-02-15 06:20:40 +0100

    confirmation mdp ok

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -20,6 +20,7 @@ export class InscriptionComponent implements OnInit {
 
   private inscriptionForm: FormGroup;
   private errorMessage: string;
+  private submitted = false;
 
   constructor(
     private fb: FormBuilder,
@@ -48,9 +49,56 @@ export class InscriptionComponent implements OnInit {
     }, {
         validator: MustMatch('password', 'confirmPassword')
       });
+    this.inscriptionForm.reset();
   }
 
+  get f() { return this.inscriptionForm.controls; }
+
   inscription() {
+    console.log("submitted" + this.submitted);
+    this.submitted = true;
+
+    if (this.inscriptionForm.invalid) {
+      let msg='';
+
+      if(this.inscriptionForm.controls.pseudo.errors.required) {
+        msg +="Champs Pseudo vide.\n";
+      }
+      if(this.inscriptionForm.controls.password.errors.required) {
+        msg += "Champs Password vide.\n";
+      }
+      if(this.inscriptionForm.controls.password.errors.minlength) {
+        msg +="Password doit contenir au moins 6 caractères.\n";
+      }
+      if(this.inscriptionForm.controls.confirmPassword.errors.required) {
+        msg +="Champs Confirmation vide.\n";
+      }
+      if(this.inscriptionForm.controls.confirmPassword.errors.mustMatch) {
+        msg +="Mot de passe différents.\n";
+      }
+      if(this.inscriptionForm.controls.age.errors.required) {
+        msg +="Champs Age vide.\n";
+      }
+      if(this.inscriptionForm.controls.sexe.errors.required) {
+        msg +="Champs Sexe vide.\n";
+      }
+      if(this.inscriptionForm.controls.ville.errors.required) {
+        msg +="Champs Ville vide.\n";
+      }
+      if(this.inscriptionForm.controls.email.errors.required) {
+        msg +="Champs Email vide.\n";
+      }
+      if(this.inscriptionForm.controls.email.errors.email) {
+        msg +="Adresse mail invalide\n";
+      }
+
+      this.submitted = false;
+
+      alert(msg);
+
+      return;
+    }
+
     let geek = new Geek();
     geek.pseudo = this.inscriptionForm.get('pseudo').value;
     geek.age = this.inscriptionForm.get('age').value;
@@ -63,6 +111,7 @@ export class InscriptionComponent implements OnInit {
       () => {
         this.inscriptionService.saveGeek(geek).then(
           () => {
+            alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.inscriptionForm.value, null, 4));
             this.authService.signOutUser();
           },
           (error) => {
@@ -77,8 +126,8 @@ export class InscriptionComponent implements OnInit {
   }
 
   resetUserForm() {
+    this.submitted = false;
     this.inscriptionForm.reset();
-    this.errorMessage = '';
   }
 
 }
