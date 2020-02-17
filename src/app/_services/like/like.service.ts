9ca454aa68885b610b123c79d112fa17d9a1fb0b
Nicolas Congard <nicolascongard@gmail.com>
2020-02-17 17:50:29 +0100

    like/superlike fonctionels

--- src/app/_services/like/like.service.ts
+++ src/app/_services/like/like.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {Photo} from '../../_models/photo';
+import { Photo } from '../../_models/photo';
+import { Action } from 'src/app/_models/action';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class LikeService {
-	private baseUrl = 'http://localhost:8080/geek/';
-  private baseUrlPhoto = 'http://localhost:8080/photo/album/';
+	private baseUrl = 'http://localhost:8080/photo/album/';
+	private baseUrlAction = 'http://localhost:8080/action/';
 
 
-  constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient) { }
 
-	getAll(): Observable<any> {
-		return this.http.get(`${this.baseUrl}`);
+	getById(id: number): Observable<Photo[]> {
+		return this.http.get<Photo[]>(`${this.baseUrl}` + id);
 	}
-  getById(id: number): Observable<Photo[]> {
-    return this.http.get<Photo[]>(`${this.baseUrlPhoto}`+id);
-  }
+
+	addNew(action: Action): Observable<Action[]> {
+		return this.http.post<Action[]>(`${this.baseUrlAction}`, action);
+	}
+
 }
